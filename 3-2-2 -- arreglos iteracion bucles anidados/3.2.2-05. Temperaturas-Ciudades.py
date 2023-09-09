2de13586b63c162a1c918af080bfba240c3246cc
gfernandez <gfernandez@uea.edu.ec>
2023-09-09 14:37:56 -0500

    Semana 12 - Iteracion de arreglos con bucles anidados

--- 3-2-2 -- arreglos iteracion bucles anidados/3.2.2-05. Temperaturas-Ciudades.py
+++ 3-2-2 -- arreglos iteracion bucles anidados/3.2.2-05. Temperaturas-Ciudades.py
@@ -1,7 +1,7 @@
 # Crear una matriz 3D para almacenar datos de temperaturas
 # Primera dimensión: Ciudades (3 ciudades)
-# Segunda dimensión: Días de la semana (7 días)
-# Tercera dimensión: Semanas (4 semanas)
+# Segunda dimensión: Semanas (4 semanas)
+# Tercera dimensión: Días de la semana (7 días)
 temperaturas = [
     [   # Ciudad 1
         [   # Semana 1
