2c4981bfdaeefa7bafb2cc055de20ccd98c18cd2
gfernandez <gfernandez@uea.edu.ec>
2023-09-18 18:26:38 -0500

    Semana 12 - Iteracion de arreglos con bucles anidados

--- 4-1-1 -- funciones, colecciones y archivos/4.1.1-02. Ejemplo 02 - Temperaturas.py
+++ 4-1-1 -- funciones, colecciones y archivos/4.1.1-02. Ejemplo 02 - Temperaturas.py
@@ -1,11 +1,9 @@
 def temperatura_promedio(ciudades_temperaturas):
     """
     Esta función calcula la temperatura promedio de un conjunto de ciudades.
-
     Args:
         ciudades_temperaturas (dict): Un diccionario que contiene nombres de ciudades como claves
                                       y listas de temperaturas como valores.
-
     Returns:
         dict: Un diccionario que contiene nombres de ciudades como claves
               y temperaturas promedio como valores.
